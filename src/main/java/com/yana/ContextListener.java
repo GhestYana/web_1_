2ea8bbd0358bbf6eec470c8507140002ce1ca1a7
Yana <166945917+GhestYana@users.noreply.github.com>
2025-11-12 21:44:12 +0200

    Update ContextListener.java

--- src/main/java/com/yana/ContextListener.java
+++ src/main/java/com/yana/ContextListener.java
@@ -13,18 +13,16 @@ public class ContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         ProductRepository repo = new ProductRepository();
 
-        // Створюємо 10 продуктів
         for (int i = 1; i <= 10; i++) {
             repo.addProduct(new Product(i, "Продукт №" + i, i * 100.0));
         }
 
-        // Зберігаємо репозиторій у контексті
         ServletContext ctx = sce.getServletContext();
         ctx.setAttribute("productRepo", repo);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        // нічого
-    }
+
+}
 }
